10fad664550f42abfc0c779cfccdf09b2dd5c354
Jens0902 <72078515+Jens0902@users.noreply.github.com>
2022-09-07 10:15:18 +0200

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,22 @@
+from time import sleep
+
+import robot
+
+# Create a robot object and initialize
+arlo = robot.Robot()
+
+print("Running ...")
+
+
+# send a go_diff command to drive forward
+leftSpeed = 64
+rightSpeed = 64
+print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+
+# Wait a bit while robot moves forward
+sleep(3)
+
+# send a stop command
+print(arlo.stop())
+
+print("Finished")
