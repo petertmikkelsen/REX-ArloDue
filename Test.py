a9733b329361d5277a33c79691ca0454f8d86f24
Jens0902 <72078515+Jens0902@users.noreply.github.com>
2022-09-07 12:17:46 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -6,44 +6,24 @@ import robot
 arlo = robot.Robot()
 
 print("Running ...")
+for i in range(4)
+  print(arlo.go_diff(64, 69, 1, 1))
 
-print(arlo.go_diff(64, 69, 1, 1))
+  sleep(2.3)
 
-sleep(2.3)
+  print(arlo.stop())
 
-print(arlo.stop())
+  sleep(1)
 
-sleep(1)
+  # send a go_diff command to drive forward
+  leftSpeed = 42
+  rightSpeed = 46
+  print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
 
-# send a go_diff command to drive forward
-leftSpeed = 42
-rightSpeed = 46
-print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
+  # Wait a bit while robot moves forward
+  sleep(1.1)
 
-# Wait a bit while robot moves forward
-sleep(2.44)
-
-# send a stop command
-print(arlo.stop())
-
-sleep(1)
-
-print(arlo.go_diff(64, 69, 1, 1))
-
-sleep(2.3)
-
-print(arlo.stop())
-
-sleep(1)
-
-# send a go_diff command to drive forward
-leftSpeed = 42
-rightSpeed = 46
-print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
-
-# Wait a bit while robot moves forward
-sleep(2.2)
-
-print(arlo.stop())
+  # send a stop command
+  print(arlo.stop())
 
 print("Finished")
