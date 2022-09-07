75701ab773f7a8e80ddb3cacf98a091025ad94c7
Jens0902 <72078515+Jens0902@users.noreply.github.com>
2022-09-07 14:35:08 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -8,7 +8,7 @@ arlo = robot.Robot()
 print("Running ...")
 
 # send a go_diff command to drive forward
-leftSpeed = 55
+leftSpeed = 50
 rightSpeed = 90
 print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
 
