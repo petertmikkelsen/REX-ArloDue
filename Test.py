1078ada58905ac6fc00b1e46bca9fe65541fa315
Jens0902 <72078515+Jens0902@users.noreply.github.com>
2022-09-07 12:20:42 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -8,11 +8,11 @@ arlo = robot.Robot()
 print("Running ...")
 for i in range(4):
   print(arlo.go_diff(64, 69, 1, 1))
-
+  
   sleep(2.3)
 
   print(arlo.stop())
-
+  break
   sleep(1) #wait before new command
 
   # send a go_diff command to drive forward
