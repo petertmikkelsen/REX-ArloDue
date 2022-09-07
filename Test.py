d4447dd20470574aeb2c408d78a941a2a910e7c5
Jens0902 <72078515+Jens0902@users.noreply.github.com>
2022-09-07 12:13:51 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -13,6 +13,8 @@ sleep(2.3)
 
 print(arlo.stop())
 
+sleep(1)
+
 # send a go_diff command to drive forward
 leftSpeed = 42
 rightSpeed = 46
@@ -24,11 +26,15 @@ sleep(2.44)
 # send a stop command
 print(arlo.stop())
 
+sleep(1)
+
 print(arlo.go_diff(64, 69))
 
 sleep(2.3)
 
 print(arlo.stop())
+
+sleep(1)
 
 # send a go_diff command to drive forward
 leftSpeed = 42
@@ -37,5 +43,7 @@ print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
 
 # Wait a bit while robot moves forward
 sleep(2.2)
+
+print(arlo.stop())
 
 print("Finished")
