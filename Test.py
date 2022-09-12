438b9f6ce4cb6e937cc00e26cb2f1bfbb3e6e01e
Jens0902 <72078515+Jens0902@users.noreply.github.com>
2022-09-12 15:04:11 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -7,20 +7,14 @@ arlo = robot.Robot()
 
 print("Running ...")
 
+  
 # send a go_diff command to drive forward
-leftSpeed = 45
-rightSpeed = 90
-print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+leftSpeed = 42
+rightSpeed = 46
+print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
 
-sleep(10.5)
-
-if (True):
-  # send a go_diff command to drive forward
-  leftSpeed = 90
-  rightSpeed = 45
-  print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
-
-  sleep(6.2)
+# Wait a bit while robot moves forward
+sleep(0.9)
 
 print(arlo.stop())
   
