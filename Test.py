efa7da10a2b9f5ab8376fe3a1ed60b5483914f60
Jens0902 <72078515+Jens0902@users.noreply.github.com>
2022-09-12 15:26:29 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -10,6 +10,18 @@ print("Running ...")
 print("Front sensor = ", arlo.read_front_ping_sensor())
 sleep(0.041)
 
+# request to read Back sonar ping sensor
+print("Back sensor = ", arlo.read_back_ping_sensor())
+sleep(0.041)
+
+# request to read Right sonar ping sensor
+print("Right sensor = ", arlo.read_right_ping_sensor())
+sleep(0.041)
+
+# request to read Left sonar ping sensor
+print("Left sensor = ", arlo.read_left_ping_sensor())
+sleep(0.041)
+
 print(arlo.stop())
   
 print("Finished")
