2ad977439ae14ea6bb5f96e16333a51e2a8e9823
Jens0902 <72078515+Jens0902@users.noreply.github.com>
2022-09-14 11:50:00 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -6,13 +6,9 @@ import RobotDue
 arlo = RobotDue.Robot()
 
 lastturn = True #variable for when the robot should turn any direction (left or right)
-
+  
 for k in range(10):
   #arlo.Forward(compensate = True)
-  print(arlo.go_diff(64, 70, 1, 1))
-  sleep(2.45)
-  print(arlo.stop())
-  sleep(0.1)
   pings = [] #front, back ,left, right
   for i in range(4):
     pings.append(arlo.read_sensor(i))
@@ -21,12 +17,18 @@ for k in range(10):
   pings = [x<1500 for x in pings]
   if pings[2] and not pings[3]:
     arlo.Turn(False)
-  if pings[3] and not pings[2]:
+  elif pings[3] and not pings[2]:
     arlo.Turn()
-  if pings[0] and pings[2] and pings[3]:
+  elif pings[0] and pings[2] and pings[3]:
     arlo.Turn(degrees = 180)       
-  if pings[0]:
+  elif pings[0]:
     arlo.Turn(lastturn)
     lastturn = not(lastturn)
+  else:
+    print(arlo.go_diff(64, 70, 1, 1))
+    sleep(2.45)
+    print(arlo.stop())
+    sleep(0.1)
+  
   
 print("Finished")
