7856f3d88af0dbea07a9c61295b9998479bdd5f9
Timmovich <79904913+Timmovich@users.noreply.github.com>
2022-09-21 11:48:33 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -5,7 +5,7 @@ import RobotDue
 # Create a robot object and initialize
 arlo = RobotDue.Robot()
 
-arlo.Forward(compensate=True, distance=2) #Go
-  
-  
-print("Finished")
+# arlo.Forward(compensate=True, distance=2) #Go
+# print("Finished")
+
+print(arlo.read_front_ping_sensor())
