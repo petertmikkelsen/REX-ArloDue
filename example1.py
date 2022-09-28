f48af91e1769f4e7666c285f14e3462beb642113
Timmovich <79904913+Timmovich@users.noreply.github.com>
2022-09-28 15:49:05 +0200

    Update example1.py

--- example1.py
+++ example1.py
@@ -47,7 +47,7 @@ while cv2.waitKey(4) == -1: # Wait for a key pressed event
     arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
     arucoParams = cv2.aruco.DetectorParameters_create()
     (corners, ids, rejected) = cv2.aruco.detectMarkers(frameReference, arucoDict, parameters=arucoParams)
-    if (ids.any() is not None):
+    if (ids is not None):
         cv2.aruco.drawDetectedMarkers(frameReference, corners, ids)
     
         topLefty = corners[0][0,0,1]
