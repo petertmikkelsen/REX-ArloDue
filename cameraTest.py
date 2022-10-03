c20cfcc988c10937f62ebcee86c7198912c1a50b
hrl495 <79912760+hrl495@users.noreply.github.com>
2022-10-03 14:15:19 +0200

    Update cameraTest.py

--- cameraTest.py
+++ cameraTest.py
@@ -57,9 +57,12 @@ while cv2.waitKey(4) == -1: # Wait for a key pressed event
     
     if (type(ids) is not type(None)):
         print('fundet')
-        rvecs, tvecs, markpointers= cv2.aruco.estimatePoseSingleMarkers(corners, 0.145, cameraMatrix, distCoeffs)
+        #rvecs, tvecs, markpointers= cv2.aruco.estimatePoseSingleMarkers(corners, 0.145, cameraMatrix, distCoeffs)
     else:
         print('ikke fundet')
+        arlo.Turn(degrees = 30)
+        sleep(2)
+    
     # Show frames
     #cv2.imshow(WIN_RF, frameReference)
     
