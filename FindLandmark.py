30b6ff8dd16a914309f4c3dd40ddc37919dd5d32
hrl495 <79912760+hrl495@users.noreply.github.com>
2022-10-12 10:14:33 +0200

    Update FindLandmark.py

--- FindLandmark.py
+++ FindLandmark.py
@@ -15,7 +15,7 @@ import time
   
 print("Finished")
 # 1024 x 720
-def gstreamer_pipeline(capture_width=1920, capture_height=1080, framerate=30):
+def gstreamer_pipeline(capture_width=1024, capture_height=720, framerate=30):
     """Utility function for setting parameters for the gstreamer camera pipeline"""
     return (
         "libcamerasrc !"
@@ -49,7 +49,7 @@ arlo = RobotDue.Robot()
 arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
 arucoParams = cv2.aruco.DetectorParameters_create()
 # 512 x 360
-cameraMatrix = np.matrix('1766 0 960; 0 1766 540; 0 0 1')
+cameraMatrix = np.matrix('1766 0 512; 0 1766 360; 0 0 1')
 distCoeffs = np.zeros((4,1))
 
 def FindLandmark(robot, ids_array):
