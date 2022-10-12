fe15f815810e2ce17a05f6a02902a5aeb6db52a7
Timmovich <79904913+Timmovich@users.noreply.github.com>
2022-10-12 13:01:27 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -14,30 +14,30 @@ import camera
 
 print("Finished")
 # 1024 x 720
-def gstreamer_pipeline(capture_width=1024, capture_height=720, framerate=30):
-    """Utility function for setting parameters for the gstreamer camera pipeline"""
-    return (
-        "libcamerasrc !"
-        "video/x-raw, width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
-        "videoconvert ! "
-        "appsink"
-        % (
-            capture_width,
-            capture_height,
-            framerate,
-        )
-    )
+#def gstreamer_pipeline(capture_width=1024, capture_height=720, framerate=30):
+#    """Utility function for setting parameters for the gstreamer camera pipeline"""
+#    return (
+#        "libcamerasrc !"
+#        "video/x-raw, width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+#        "videoconvert ! "
+#        "appsink"
+#        % (
+#            capture_width,
+#            capture_height,
+#            framerate,
+#        )
+#    )
 
 
-print("OpenCV version = " + cv2.__version__)
+#print("OpenCV version = " + cv2.__version__)
 
 # Open a camera device for capturing
-cam = cv2.VideoCapture(gstreamer_pipeline(), apiPreference=cv2.CAP_GSTREAMER)
+#cam = cv2.VideoCapture(gstreamer_pipeline(), apiPreference=cv2.CAP_GSTREAMER)
 
 
-if not cam.isOpened(): # Error
-    print("Could not open camera")
-    exit(-1)
+#if not cam.isOpened(): # Error
+#    print("Could not open camera")
+#    exit(-1)
 
 # Open a window
 #WIN_RF = "Example 1"
